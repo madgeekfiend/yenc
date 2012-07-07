c4779c8a43a7ae69ca3b8a4616938fa05af21c83
Sam <sam.contapay@gmail.com>
2012-07-07 04:07:33 -0700

    Progress with something actually being written to binary format.

--- lib/y_enc.rb
+++ lib/y_enc.rb
@@ -10,33 +10,47 @@ class YEnc
     if is_yenc?
             #Continue decoding
       puts "File is verified as yEncoded file. Beginning decoding."
-      File.open(@filepath, 'r') do |f|
-        f.any? do |line|
+      begin_read = false
+
+      File.open(@filepath, 'r').each_line do |line|
+
+          puts "BEGIN_READ #{begin_read}"
           if line.include?("=ybegin") #This is the begin size
             breakdown_header line
+            begin_read = true
             next
           end
-          #end of reading lines
-          #Decode and write to binary file
-          newFile = File.new(@filename, "w")
-          escape = false
-
-          line.each_byte do |c|
-            if c == 61 #escape character hit goto the next one
-              escape = true
-              next
-            elsif escape
-              escape = false
-              decoded = c - 64
-            else
-              decoded = c - 42
-            end
-            newFile.putc decoded
+          if line.include?("=yend")
+            puts "Reached the end of file"
+            break #stop looking through the file we are done
           end
-          #done writing close the file
-          newFile.close
-        end
+          #end of reading lines
+
+          if begin_read
+            puts "Line count #{line.length}"
+            #Decode and write to binary file
+            esc = false
+            line.each_byte do |c|
+              if c == 61 #escape character hit goto the next one
+                esc = true
+                next
+              elsif esc
+                esc = false
+                decoded = c - 64
+              else
+                decoded = c - 42
+              end
+
+              if decoded.between?(0,41)
+                decoded = decoded + 214
+              end
+              #if decoded is 0 - 41 add 214
+              @new_file.putc decoded
+            end
+          end
+
       end
+      @new_file.close
     else
       puts "This is not a yEncoded file."
     end
@@ -54,6 +68,11 @@ class YEnc
     @line=line[/line=([^\s]+)/,1] if @line.nil?
     puts "File Name: #{@filename}"
     puts "Size: #{@filesize} Length: #{@line}"
+    puts "Creating file: #{@outputpath}#{@filename}"
+    @new_file = File.new(@outputpath + @filename, "wb")
   end
 
 end
+
+y = YEnc.new("/Users/scontapay/yenc.txt", "/Users/scontapay/")
+y.decode
