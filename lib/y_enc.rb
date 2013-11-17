e3ba95463178492e3ab582bfe93ec3b44e7e9c0e
Tadeus Dobrovolskij <admin@tad-do.net>
2013-11-17 23:43:16 +0100

    10% performance gain in decoder.

--- lib/y_enc.rb
+++ lib/y_enc.rb
@@ -72,49 +72,50 @@ class YEnc
 
       File.open(@filepath, 'r').each_line do |line|
 
-          if line.include?("=ybegin") #This is the begin size
-            breakdown_header line
-            begin_read = true
-            next
-          end
-          if line.include?("=yend")
+        if begin_read
+          if line.start_with?("=yend")
             breakdown_endline line
             begin_read=false
             break #stop looking through the file we are done
           end
           #end of reading lines
 
-          if begin_read
-            #puts "LINE COUNT: #{line.length}"
-            #Decode and write to binary file
-            esc = false
-            line.each_byte do |c|
-              next if c == 13 or c == 10
+          #puts "LINE COUNT: #{line.length}"
+          #Decode and write to binary file
+          esc = false
+          line.each_byte do |c|
+            next if c == 13 or c == 10
 
-              if c == 61 and not esc #escape character hit goto the next one
-                esc = true
-                next
-              else
-                if esc
-                  esc = false
-                  c = c - 64
-                end
-
-                if c.between?(0,41)
-                  decoded = c + 214
-                else
-                  decoded = c - 42
-                end
+            if c == 61 and not esc #escape character hit goto the next one
+              esc = true
+              next
+            else
+              if esc
+                esc = false
+                c = c - 64
               end
-              @new_file.putc decoded
+
+              if c.between?(0,41)
+                decoded = c + 214
+              else
+                decoded = c - 42
+              end
             end
+            @new_file.putc decoded
           end
 
+        else
+          if line.start_with?("=ybegin") #This is the begin size
+            breakdown_header line
+            begin_read = true
+            next
+          end
+        end
       end
-      @new_file.close
     else
       false
     end
+    @new_file.close
   end
 
   #Does this pass the crc32 check
